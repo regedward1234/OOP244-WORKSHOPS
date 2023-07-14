2a92e4b32e206bc75e9d8ffd19846eb43ede1dff
regedward1234 <regedward1234@hotmail.com>
2023-07-14 10:46:43 -0400

    added first five functions

--- Project MileStones/MS2/LibApp.cpp
+++ Project MileStones/MS2/LibApp.cpp
@@ -1,6 +1,64 @@
-
+/***********************************************************************
+// OOP244 Menu Module
+// File	LibApp.cpp
+// Version: 1.0
+// Date: July 14th 2023
+// Author: Reginald Edward
+// Description: To demo the execution of the Seneca Library Application with printing messages
+//
+// Revision History
+// -----------------------------------------------------------
+// Name            Date            Reason
+//Reginald         2023/07/14
+/////////////////////////////////////////////////////////////////
+I have done all the coding by myself and only copied the code
+that my professor provided to complete my workshops and assignments.
+-----------------------------------------------------------------------
+***********************************************************************/
+#define _CRT_SECURE_NO_WARNINGS
+#include <iostream>
 #include "LibApp.h"
-namespace sdds {
-   
 
+using namespace std;
+namespace sdds {
+
+	bool LibApp::confirm(const char* message) {
+		bool temp;
+
+		// create an object of Menu and add yes to it using overloaded operator <<
+		Menu menu(message);
+		menu << "Yes";
+
+		// run the menu and return true if run methods returns 1
+		if (menu.run() == 1) {
+			temp = true;
+		}
+		else {
+			temp = false;
+		}
+		return temp;
+	}
+
+	void LibApp::load() {
+		// prints line
+		cout << "Loading Data" << endl;
+	}
+
+	void LibApp::save() {
+		// prints line
+		cout << "Saving Data" << endl;
+	}
+
+	void LibApp::search() {
+		// prints line
+		cout << "Searching for publication" << endl;
+	}
+
+	void LibApp::returnPub() {
+		// call search function, prints lines, change value of m_changed to true
+		search();
+		cout << "Returning publication" << endl;
+		cout << "Publication returned" << endl;
+		m_changed = true;
+	}
 }
--- Project MileStones/MS2/LibApp.h
+++ Project MileStones/MS2/LibApp.h
@@ -1,3 +1,20 @@
+/***********************************************************************
+// OOP244 Menu Module
+// File	LibApp.h
+// Version: 1.0
+// Date: July 14th 2023
+// Author: Reginald Edward
+// Description: To demo the execution of the Seneca Library Application with printing messages
+//
+// Revision History
+// -----------------------------------------------------------
+// Name            Date            Reason
+//Reginald         2023/07/14      Added the prototypes for all the functions
+/////////////////////////////////////////////////////////////////
+I have done all the coding by myself and only copied the code
+that my professor provided to complete my workshops and assignments.
+-----------------------------------------------------------------------
+***********************************************************************/
 #ifndef SDDS_LIBAPP_H
 #define SDDS_LIBAPP_H
 #include "Menu.h"
