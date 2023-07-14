624b14e542350f980224097e7ba56e69e439fea5
regedward1234 <regedward1234@hotmail.com>
2023-07-14 10:25:19 -0400

    added prototypes for the functions

--- Project MileStones/MS2/LibApp.h
+++ Project MileStones/MS2/LibApp.h
@@ -7,6 +7,18 @@ namespace sdds {
 	   bool m_changed;
 	   Menu m_mainMenu;
 	   Menu m_exitMenu;
+	   bool confirm(const char* message); //instantiate a Menu and initialize it with message argument
+       void load(); // prints: "Loading Data"<NEWLINE>
+       void save(); // prints: "Saving Data"<NEWLINE>
+       void search(); // prints: "Searching for publication"<NEWLINE>
+       void returnPub(); //  Calls the search() method.
+   public:
+       LibApp();
+       void newPublication(); // adds new publication to library
+       void removePublication(); // removes publication from library
+       void checkOutPub(); // calls search then confirm methods
+       void run();
+                         
    };
 }
 #endif // !SDDS_LIBAPP_H
